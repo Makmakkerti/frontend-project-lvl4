2e7807fccf85aea6314ed8b02ff217f69f2e0c14
makmakkerti <makmakkerti007@gmail.com>
2021-01-29 18:35:19 +0200

    add message to current channel with axios

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import Cookies from 'js-cookie';
+import faker from 'faker';
 import Channels from './Channels';
 import Messages from './Messages';
+
+if (!Cookies.get('username')) {
+  const fakeName = faker.fake('{{name.firstName}}_{{name.lastName}}');
+  Cookies.set('username', fakeName);
+}
+
+const username = Cookies.get('username');
 
 const App = ({ data }) => (
   <div className="row h-100 pb-3">
     <Channels data={data} />
-    <Messages />
+    <Messages data={data} username={username} />
   </div>
 );
 
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,45 +1,68 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
+import axios from 'axios';
 
-const Messages = () => (
-  <div className="col h-100">
-    <div className="d-flex flex-column h-100">
-      <div id="messages-box" className="chat-messages overflow-auto mb-3">
-        <div className="text-break">
-          <b>Lamar_Ritchie39</b>
-          : hi
-        </div>
-        <div className="text-break">
-          <b>Lamar_Ritchie39</b>
-          : hello
-        </div>
-        <div className="text-break">
-          <b>Layne31</b>
-          : Hhg
-        </div>
-        <div className="text-break">
-          <b>Layne31</b>
-          : Yet
-        </div>
-      </div>
-      <div className="mt-auto">
-        <form noValidate className="">
-          <div className="form-group">
-            <div className="input-group">
-              <input name="body" aria-label="body" className="mr-2 form-control" />
-              <button
-                aria-label="submit"
-                type="submit"
-                className="btn btn-primary"
-              >
-                Submit
-              </button>
-              <div className="d-block invalid-feedback">&nbsp;</div>
+const Messages = ({ username, data }) => {
+  const { currentChannelId, messages } = data;
+  const channelMessages = messages.filter((m) => m.channelId === currentChannelId);
+
+  const handleAddMessage = (e) => {
+    e.preventDefault();
+    const input = e.target.elements.body;
+    const message = input.value;
+
+    const currentChannelMessagesUrl = `http://localhost:5000/api/v1/channels/${currentChannelId}/messages`;
+    const messageData = {
+      data: {
+        attributes: {
+          body: message,
+          channelId: currentChannelId,
+          nickname: username,
+        },
+      },
+    };
+
+    axios.post(currentChannelMessagesUrl, messageData)
+      .then((response) => {
+        input.value = '';
+        console.log(response, data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="col h-100">
+      <div className="d-flex flex-column h-100">
+        <div id="messages-box" className="chat-messages overflow-auto mb-3">
+          {channelMessages.map((m) => (
+            <div className="text-break" key={m.id}>
+              <b>{m.nickname}</b>
+              : {m.body}
             </div>
-          </div>
-        </form>
+          ))}
+        </div>
+        <div className="mt-auto">
+          <form noValidate className="" onSubmit={handleAddMessage}>
+            <div className="form-group">
+              <div className="input-group">
+                <input name="body" aria-label="body" className="mr-2 form-control" />
+                <button
+                  aria-label="submit"
+                  type="submit"
+                  className="btn btn-primary"
+                >
+                  Submit
+                </button>
+                <div className="d-block invalid-feedback">&nbsp;</div>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Messages;
