88d44036648af29cf4aca93525a1e24c707bdc61
makmakkerti <makmakkerti007@gmail.com>
2021-02-06 11:18:26 +0200

    InputForm component added

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import axios from 'axios';
 import { connect } from 'react-redux';
+import InputForm from './InputForm';
 
 const mapStateToProps = (state) => ({
   currentChannelId: state.currentChannelId,
@@ -9,31 +9,6 @@ const mapStateToProps = (state) => ({
 
 const Messages = ({ username, currentChannelId, messages }) => {
   const channelMessages = messages.filter((m) => m.channelId === currentChannelId);
-
-  const handleAddMessage = (e) => {
-    e.preventDefault();
-    const input = e.target.elements.body;
-    const message = input.value;
-
-    const currentChannelMessagesUrl = `/api/v1/channels/${currentChannelId}/messages`;
-    const messageData = {
-      data: {
-        attributes: {
-          body: message,
-          channelId: currentChannelId,
-          nickname: username,
-        },
-      },
-    };
-
-    axios.post(currentChannelMessagesUrl, messageData)
-      .then(() => {
-        input.value = '';
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
 
   return (
     <div className="col h-100">
@@ -49,21 +24,7 @@ const Messages = ({ username, currentChannelId, messages }) => {
           ))}
         </div>
         <div className="mt-auto">
-          <form noValidate className="" onSubmit={handleAddMessage}>
-            <div className="form-group">
-              <div className="input-group">
-                <input name="body" aria-label="body" className="mr-2 form-control" />
-                <button
-                  aria-label="submit"
-                  type="submit"
-                  className="btn btn-primary"
-                >
-                  Submit
-                </button>
-                <div className="d-block invalid-feedback">&nbsp;</div>
-              </div>
-            </div>
-          </form>
+          <InputForm username={username} />
         </div>
       </div>
     </div>
