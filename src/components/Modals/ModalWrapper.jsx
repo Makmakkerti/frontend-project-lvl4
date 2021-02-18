2e6d7fda2b425b73460d34e6ef1de49b54b3492d
makmakkerti <makmakkerti007@gmail.com>
2021-02-18 14:38:42 +0200

    Modals changed connect to hooks, ModalWrapper added

---
+++ src/components/Modals/ModalWrapper.jsx
@@ -0,0 +1,23 @@
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Modal } from 'react-bootstrap';
+import { closeModal } from '../../store/modal';
+
+const ModalWrapper = ({ children, title }) => {
+  const dispatch = useDispatch();
+  const modalState = useSelector((state) => state.modalState);
+  const handleClose = useCallback(() => dispatch(closeModal()), [dispatch]);
+
+  return (
+    <Modal show={modalState.opened} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>{title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {children}
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default ModalWrapper;
