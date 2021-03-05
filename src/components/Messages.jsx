592744d09d59dbd326a10e187036bfa36f2204f3
makmakkerti <makmakkerti007@gmail.com>
2021-03-05 15:20:20 +0200

    Scroll to new messages

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import InputForm from './InputForm';
 
@@ -6,11 +6,24 @@ const Messages = ({ i18next }) => {
   const currentChannelId = useSelector((state) => state.currentChannelId);
   const channelMessages = useSelector((state) => state.messages
     .filter((m) => m.channelId === currentChannelId));
+  const chatContainer = React.createRef();
+
+  const scrollToNewMessage = () => {
+    const { current } = chatContainer;
+    const diff = current.scrollHeight - current.clientHeight;
+    if (diff > 0) {
+      current.scrollTo(0, diff, 'smooth');
+    }
+  };
+
+  useEffect(() => {
+    scrollToNewMessage();
+  });
 
   return (
     <div className="col h-100">
       <div className="d-flex flex-column h-100">
-        <div id="messages-box" className="chat-messages overflow-auto mb-3">
+        <div id="messages-box" ref={chatContainer} className="chat-messages overflow-auto mb-3">
           {channelMessages.map((m) => (
             <div className="text-break" key={m.id}>
               <b>{m.nickname}</b>
